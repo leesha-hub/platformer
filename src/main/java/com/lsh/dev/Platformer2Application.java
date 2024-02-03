5f285bc30a58a24c1e8ddb9abf1451ac8e6aa25c
leesha1223 <leesha1223@naver.com>
2024-02-03 09:22:54 +0900

    first commit

---
+++ src/main/java/com/lsh/dev/Platformer2Application.java
@@ -0,0 +1,13 @@
+package com.lsh.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Platformer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Platformer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsh/dev/Platformer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsh.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Platformer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
